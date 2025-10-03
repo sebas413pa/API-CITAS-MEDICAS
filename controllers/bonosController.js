324e182f26f0a226c981aa0673dd9ad9309ed3eb
sebas413pa <sebasic_7@hotmail.com>
2025-10-03 12:57:15 -0600

    feat(bonos): Función de crear bono
    Función de crear bono

--- controllers/bonosController.js
+++ controllers/bonosController.js
@@ -36,20 +36,39 @@ module.exports = {
             endDate.setHours(23, 59, 59, 999);
 
             const results = await Citas.findAll({
-                attributes: [
-                    'medicos_id_medico',
-                    [fn('COUNT', fn('DISTINCT', col('pacientes_id_paciente'))), 'pacientes_count']
-                ],
-                where: {
-                    fecha: {
-                        [Op.gte]: monday,
-                        [Op.lte]: endDate
-                    },
-                    estado: 'completada' 
-                },
-                group: ['medicos_id_medico'],
-                having: sequelize.where(fn('COUNT', fn('DISTINCT', col('pacientes_id_paciente'))), Op.gt, 50)
+            attributes: [
+                'medicos_id_medico',
+                [fn('COUNT', fn('DISTINCT', col('pacientes_id_paciente'))), 'pacientes_count']
+            ],
+            where: {
+                fecha: { [Op.gte]: monday, [Op.lte]: endDate },
+                estado: 'completada'
+            },
+            group: ['medicos_id_medico'],
+            having: sequelize.where(fn('COUNT', fn('DISTINCT', col('pacientes_id_paciente'))), Op.gt, 50)
             });
+            if (results.length === 0) {
+                return res.status(200).json({
+                    success: true,
+                    message: "No hay médicos con más de 50 pacientes únicos desde el lunes. No se crearon bonos.",
+                    data: []
+                });
+            }
+
+            const createdBonos = [];
+            for (const result of results) {
+                const { medicos_id_medico, pacientes_count } = result.dataValues;
+
+                const newBono = await Bono.create({
+                    medicos_id_medico: medicos_id_medico,
+                    monto: 100,                        
+                    cantidad_pacientes: pacientes_count,
+                    periodo_inicio: monday,            
+                    periodo_final: endDate
+                });
+
+                createdBonos.push(newBono);
+                }
             res.status(200).json({
                 success: true,
                 message: "Bonos creados exitosamente",
@@ -57,9 +76,9 @@ module.exports = {
             });
         }catch(error){
             console.log(error);
-            res.status(500).json({
+            res.status(500).json({  
                 success: false,
-                message: "No se pudo crear el bono"
+                message: "No se pudieron crear los bonos"
             });
         }
     }
